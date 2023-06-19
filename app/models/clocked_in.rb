d3e9ee0e5e43322df939a28c5c956007715e4c62
Kemila Isdiandini <kemil07@gmail.com>
2023-06-19 15:56:26 +0700

    Add tables relation and seeds

--- app/models/clocked_in.rb
+++ app/models/clocked_in.rb
@@ -1,2 +1,6 @@
 class ClockedIn < ApplicationRecord
+  belongs_to :user
+
+  validates :user_id, presence: true
+  validates :user_id, uniqueness: { scope: :end_date, message: "should end the current time tracking"}, on: :create
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+users = User.create([{ name: "Jerry" }, { name: "Tom" }, { name: "Joe" }, { name: "Dave" }])
+ClockedIn.create({start_date: 1.days.from_now, end_date: DateTime.yesterday, user: users.first})
+ClockedIn.create({start_date: DateTime.yesterday, end_date: DateTime.current, user: users[1]})
+Follow.create({follower: users.last, following: users.first})
+Follow.create({follower: users[3], following: users.first})
