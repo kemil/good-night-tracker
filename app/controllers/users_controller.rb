c4b2cfe4340fdd69c71caae2dffcd8c0bc6c1ae7
Kemila Isdiandini <kemil07@gmail.com>
2023-06-19 22:40:16 +0700

    Add follow and unfollow users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,6 +58,38 @@ class UsersController < ApplicationController
     end
   end
 
+  def follow
+    @user = User.find(params[:id])
+    @following = User.find(params[:following_id])
+
+    respond_to do |format|
+      unless @user.followings.include?(@following)
+        @user.followings << @following
+        format.html { redirect_to user_url(@user), notice: "User was successfully follow #{@following.name}." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: "#{@user.name} already follow #{@following.name}.", status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def unfollow
+    @user = User.find(params[:id])
+    @followed_users = @user.followed_users.find_by(following_id: params[:following_id])
+
+    respond_to do |format|
+      if @followed_users
+        @followed_users.destroy
+        format.html { redirect_to user_url(@user), notice: "User was successfully unfollow #{@followed_user.name}." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: "Unsuccessfully unfollow #{@user.name}.", status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :follows
   resources :clocked_ins
   resources :users
+
+  post '/users/:id/follow/:following_id', to: "users#follow", as: "follow_user"
+  post '/users/:id/unfollow/:following_id', to: "users#unfollow", as: "unfollow_user"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
